09509feaaa44ce166e7d4f51cc5c3a4f19de1102
Serhii <88855803+Jim4Support@users.noreply.github.com>
2022-09-01 18:16:14 +0300

    Update Footer.js
    need to fix form

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,39 +1,66 @@
 import React, {useState} from 'react';
 import './Footer.css'
 
-export default function Footer() {
-    const inc = (init = 0) => () => ++init
-  const genId = inc();
-   /*  const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState([]);
+export default function Footer({tasks, setTasks}) {
+ 
+  const [newTask, setNewTask] = useState('');
+  const [newDesc, setNewDesc] = useState('');
+
+  // const handleInputChange = (event) => { //example
+  //   const target = event.target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   const name = target.name;
+
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // }
 
   function addTask() {
-    if (!newTask) {
-      alert('Please, enter a task');
-      return;
+    setTasks([...tasks, {
+      name: newTask
     }
-    const task = {
-      id: genId(),
-      value: newTask,
-    };
-
-    setTasks((oldList) => [...oldList, task]);
-    setNewTask("");
+  ]);
+  setNewTask('');
   }
-    <div className="app">
-      <h1>My Todo List</h1>
-      <input type="text" placeholder="Add a new task..." value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}/>
-      <button onClick={() => addTask()}>Add</button>
-    </div> */
+
+  // function addDesc() {
+  //   setTasks([...tasks, {
+  //     description: newDesc
+  //   }
+  // ]);
+  // setNewDesc('');
+  // }
+ 
+  const nameAndDesc = (event) => {
+    event.preventDefault();
+    addTask()
+    // addDesc()
+  }
+//   tasksForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     if (inputTask[0].value.trim() === null || inputTask[0].value.trim() === '') {
+//         errorInput.innerText = 'Please, enter a task';
+//     } else {
+//         errorInput.innerText = '';
+//         const formData = new FormData(tasksForm);
+//         const formTask = Object.fromEntries(formData.entries());
+//         data.push(new Task(data.id, onPushDate(inputDate[0].value), false, inputTask[0].value, inputDesc[0].value.trim()));
+//         addTask(formTask).then(generateTasks)
+//             .then(_ => tasksForm.reset())
+//         renderTask();
+//     }
+// });
+
+
     return (
-        <footer className='appfooter'>
-            <form name="task-input" id="form">
-                <input name="dueDate" type="date" />
-                <input name="name" type="text" placeholder="Add a new task" />
-                <span id="errorInput"></span>
-                <input name="description" type="text" placeholder="Add a description" />
-                <button name="submit-task" type="submit">Add</button>
+        <footer className='footer'>
+            <form onSubmit={nameAndDesc}>
+                <input name="dueDate" type="date"/>
+                <input name="name" type="text" placeholder="Add a new task" value={newTask} onChange={(t) => setNewTask(t.target.value)}/>
+                {/* <span id="errorInput"></span> */}
+                <input name="description" type="text" placeholder="Add a description" value={newDesc} onChange={(t) => setNewDesc(t.target.value)}/>
+                <input type="submit" value="Add"/>
             </form>
         </footer>
     )
