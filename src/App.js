c1aa842cfb303940489adc334ac4c5e4e22adc9e
Serhii <88855803+Jim4Support@users.noreply.github.com>
2022-09-01 18:13:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Main from './Components/Main/Main.js';
 import Header from './Components/Header/Header.js';
@@ -6,45 +6,50 @@ import Footer from './Components/Footer/Footer.js';
 
 function App() {
   
-  /* class Task {
-    constructor(id, dueDate, done, name, description) {
-      if (typeof name === 'object') {
-        Object.assign(this, name);
-      } else {
-        this.id = genId();
-        this.dueDate = dueDate || null;
-        this.done = done || false;
-        this.name = name;
-        this.description = description || null;
-      }
-    }
-
-    toString() {
-      return `${this.id} ${this.dueDate} ${this.done} ${this.name} ${this.description}`;
-    }
-  } */
-
-  /* let state = [
-    new Task(1, new Date('2022-08-29'), false, 'hometask', null),
-    new Task(2, new Date('2022-08-30'), false, 'officetask', 'make office work'),
-    new Task(3, new Date('2022-08-29'), true, 'shopping', 'buy vegetables'),
-    new Task(4, null, true, 'walk', 'go for a walk'),
-    new Task(5, new Date('2022-08-26'), false, 'CSS Figma', 'to do adaptive mobile first'),
-  ]; */
-
-  
-
-  // function deleteTask(id) {
-  //   const newArray = tasks.filter((t) => t.id !== id);
-  //   setTasks(newArray);
-  // }
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      dueDate: '2022-09-02',
+      done: false,
+      name: 'hometask',
+      description: null
+    },
+    {
+      id: 2,
+      dueDate: '2022-09-02',
+      done: false,
+      name: 'officetask',
+      description: 'make office work'
+    },
+    {
+      id: 3,
+      dueDate: '2022-08-21',
+      done: true,
+      name: 'shopping',
+      description: 'buy vegetables'
+    },
+    {
+      id: 4,
+      dueDate: null,
+      done: true,
+      name: 'walk',
+      description: 'go for a walk'
+    },
+    {
+      id: 5,
+      dueDate: '2022-08-26',
+      done: false,
+      name: 'CSS Figma',
+      description: 'to do adaptive mobile first'
+    },
+  ]);
 
   return (
     <div className="todoapp">
       <Header />
       <div className='content'>
-        <Main />
-        <Footer />
+        <Main tasks={tasks} setTasks={setTasks}/>
+        <Footer tasks={tasks} setTasks={setTasks}/>
       </div>
     </div>
   );
